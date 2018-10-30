f79c0837e399029a8b77c092debafd1a565aca42
Maxim Lamare <maxim.lamare@univ-grenoble-alpes.fr>
2018-10-30 16:20:58 +0100

    Optimise and document code

--- snappy_funcs.py
+++ snappy_funcs.py
@@ -10,11 +10,16 @@ from snappy import ProductIO, GeoPos, PixelPos, HashMap, GPF
 
 
 def open_prod(inpath):
-    """ Use SNAP to open a Sentinel 3 product.
-        INPUT: pathlib object pointing to S3 xfdumanifest (unzip downloaded
-        file).
-        OUTPUT: snappy product """
+    """Open SNAP product.
 
+     Use SNAP to open a Sentinel 3 product.
+
+    Args:
+        inpath (str): Path to a S3 OLCI image xfdumanisfest.xml file
+
+    Returns:
+        (java.lang.Object): snappy java object: SNAP image product
+     """
     # Open satellite product with SNAP
     try:
         prod = ProductIO.readProduct(inpath)
@@ -25,22 +30,26 @@ def open_prod(inpath):
     return prod
 
 
-def pixel_position(in_prod, inlat, inlon):
-    """ Extract the pixel position from a product, based on lat / lon
-        coordinates
-        INPUTS:
-        - in_prod: snappy product
-        - inlat: latitude
-        - inlon: longitude
-        OUTPUTS:
-        - xx, yy: tuple of pixel coordinates
-        Returns (None, None) if coordinates are out of bounds"""
+def pixel_position(inprod, inlat, inlon):
+    """Get pixel position in a product.
+
+    Extract the pixel position from a product, based on lat / lon coordinates.
+
+    Args:
+        inprod (java.lang.Object): SNAP image product
+        inlat (float): latitude of the coordinate in degrees EPSG:4326
+        inlon (float): longitude of the coordinate in degrees EPSG:4326
+
+    Returns:
+        (tuple): pixel coordinates xx: (int), yy: (int). Returns "None" if\
+                 pixel is out of bounds.
+    """
 
     # Read lat/lon position into a GeoPos item
     gpos = GeoPos(inlat, inlon)
 
     # Retrieve pixel position of lat/lon values
-    pixpos = in_prod.getSceneGeoCoding().getPixelPos(gpos, PixelPos())
+    pixpos = inprod.getSceneGeoCoding().getPixelPos(gpos, PixelPos())
 
     # If the pixpos coordinates are NaN, the queried position is outside of the
     # image bands
@@ -50,46 +59,54 @@ def pixel_position(in_prod, inlat, inlon):
 
     else:
 
-        """ Get pixel position in the product and retrieve pixel position of
-        lat/lon values."""
+        # Get pixel position in the product and retrieve pixel position of
+        # lat/lon values.
         xx = int(pixpos.getX())
         yy = int(pixpos.getY())
 
     return (xx, yy)
 
 
-def subset(in_prod, inlat, inlon, copyMetadata="true"):
-    """ Create a subset of the given product around the point of interest.
-        3x3 pixels, and returns the pixel positions of the coordinates for the
-        subset.
-        Get area of interest from the coordinates, then subset.
-        INPUTS:
-        - in_prod: snappy product
-        - inlat: latitude
-        - inlon: longitude
-        - copyMetadata: Whether to copy the metadata of the source product.
-        default = True.
-        OUTPUTS:
-        - prod_subset: subset snappy product
-        - xx, yy: tuple of pixel coordinates
-        """
+def subset(inprod, inlat, inlon, subset_size=3, copyMetadata="true"):
+    """Subset a S3 scene opened in snappy around lat lon coordinates.
+
+    Create a subset of the given product around the point of interest. The
+     subset is 3x3 pixels, and returns the pixel positions of the coordinates
+     for the subset.
+
+     Args:
+        inprod (java.lang.Object): snappy java object: SNAP image product
+        inlat (float): latitude of the coordinate in degrees EPSG:4326
+        inlon (float): longitude of the coordinate in degrees EPSG:4326
+        subset_size (int): size of the x by x window to subset. Default = 3.
+        copyMetadata (bool): flag to copy Metadata in the output product, true\
+                             by default.
+
+    Returns:
+        (tuple): tuple containing:
+            prod_subset (java.lang.Object): snappy 3x3 subset around coordinate
+            subx, suby (tuple): pixel coordinates: xx, yy.
+    """
     # Get pixel position in the product and retrieve x,y.
-    xx, yy = pixel_position(in_prod, inlat, inlon)
+    xx, yy = pixel_position(inprod, inlat, inlon)
 
     # If problem with geocoding return None
     if not xx or not yy:
         prod_subset = subx = suby = None
 
     else:
-
         # Subset around point
-        # Area to subset (3x3 pixels)
-        area = [xx - 3, yy - 3, 6, 6]
+        area = [
+            xx - subset_size,
+            yy - subset_size,
+            subset_size * 2,
+            subset_size * 2,
+        ]
 
         # Empty HashMap
         parameters = HashMap()
 
-        # Convert area list to string
+        # Convert area list to string readable by the processor
         areastr = ",".join(str(e) for e in area)
 
         # Subset parameters
@@ -99,34 +116,91 @@ def subset(in_prod, inlat, inlon, copyMetadata="true"):
         parameters.put("copyMetadata", copyMetadata)
 
         # Create subset using operator
-        prod_subset = GPF.createProduct("Subset", parameters, in_prod)
+        prod_subset = GPF.createProduct("Subset", parameters, inprod)
 
         # Get pixel position in the subset (and therefore other products)
-        # subx, suby = pixel_position(prod_subset, inlat, inlon)
-        # Since the subset is a fixed area, the position is known
-        subx = 3
-        suby = 3
+        subx, suby = pixel_position(prod_subset, inlat, inlon)
+
     return prod_subset, (subx, suby)
 
 
+def rad2refl(
+    inprod,
+    sensor="OLCI",
+    mode="RAD_TO_REFL",
+    tpg="False",
+    flags="False",
+    nonspec="False",
+):
+    """ Radiance to Reflectance.
+
+    Convert a Sentinel 3 OLCI L1C radiance bands to TOA reflectance.
+
+     Args:
+        inprod (java.lang.Object): snappy java object: SNAP image product
+        sensor (str): satellite sensor that produced the input scene
+        tpg (str): include or not the TiePointGrids in output
+        flags (str): include or not the Product flags in output
+        nonspec (str): include or not the non spectral bands in output
+
+    Returns:
+        toa_refl (java.lang.Object): snappy TOA reflectance product
+    """
+
+    # Empty HashMap
+    parameters = HashMap()
+
+    # Put parameters for snow albedo processor
+    parameters.put("sensor", sensor)
+    parameters.put("conversionMode", mode)
+    parameters.put("copyTiePointGrids", tpg)
+    parameters.put("copyFlagBandsAndMasks", flags)
+    parameters.put("copyNonSpectralBands", nonspec)
+
+    toa_refl = GPF.createProduct("Rad2Refl", parameters, inprod)
+
+    return toa_refl
+
+
 def snap_snow_albedo(
-    prod,
+    inprod,
     pollution_flag,
     pollution_delta,
     gains,
-    ndsi_flag="false",
+    ndsi_flag="false",  # NDSI flag
     ndsi_thres="0.03",  # NDSI threshold
     pollution_params="false",  # Write pollution parameters
-    pollution_uncertainties="false",  # Write pollution uncertainties
+    pollution_uncertainties="false",  # Write pollution uncert
     deltabrr="0.01",  # Delta rBRR for uncertainties
     ppa_flag="false",  # Calculate PPA
     copyrefl="true",  # Copy rBRR bands to product
     refwvl="1020.0",  # Reference wvl for albedo calculation
     cloud_mask_name="cloud_over_snow",
 ):
+    """Snow Albedo Processor v2.0.9
 
-    """ Snow Albedo Processor v2.0.6"""
+        Run the S3 SNOW processor on the snappy product with the provided
+        options and return the snappy albedo product.
 
+        Args:
+            inprod (java.lang.Object): snappy java object: SNAP image product
+            pollution_flag (bool): S3 SNOW dirty snow flag
+            pollution_delta (int): Delta value to consider dirty snow
+            gains (bool): Consider vicarious calibration gains
+            ndsi_flag (bool):
+            ndsi_thres (str):
+            pollution_params (bool):
+            pollution_uncertainties (bool):
+            deltabrr (str):
+            ppa_flag
+            copyrefl
+            refwvl
+            cloud_mask_name (str): specify the name of the cloud mask if it \
+                                   exists
+
+
+        Returns:
+            (java.lang.Object): snappy object"""
     # Set gain values and run processor
     if gains:
         gain_b1 = "0.9798"
@@ -142,8 +216,7 @@ def snap_snow_albedo(
     # Empty HashMap
     parameters = HashMap()
 
-    # Put parameters for snow albedo processor
-
+    # Parameters for snow albedo processor
     # Cloud mask name
     parameters.put("cloudMaskBandName", cloud_mask_name)
 
@@ -157,8 +230,10 @@ def snap_snow_albedo(
     parameters.put("considerSnowPollution", pollution_flag)
     parameters.put("pollutionDelta", pollution_delta)
     parameters.put("writeAdditionalSnowPollutionParms", pollution_params)
-    parameters.put("writeUncertaintiesOfAdditionalSnowPollutionParms",
-                   pollution_uncertainties)
+    parameters.put(
+        "writeUncertaintiesOfAdditionalSnowPollutionParms",
+        pollution_uncertainties,
+    )
     parameters.put("deltaBrr", deltabrr)
 
     # PPA
@@ -177,17 +252,19 @@ def snap_snow_albedo(
     parameters.put("olciGainBand21", gain_b21)
 
     # Band list for output (for the moment hard code 21 bands)
-    bandlist = "Oa01 (400 nm),Oa02 (412.5 nm),Oa03 (442.5 nm),Oa04 (490 nm)," \
-               "Oa05 (510 nm),Oa06 (560 nm),Oa07 (620 nm),Oa08 (665 nm),Oa09" \
-               " (673.75 nm),Oa10 (681.25 nm),Oa11 (708.75 nm),Oa12 (753.75 " \
-               "nm),Oa13 (761.25 nm),Oa14 (764.375 nm),Oa15 (767.5 nm),Oa16 " \
-               "(778.75 nm),Oa17 (865 nm),Oa18 (885 nm),Oa19 (900 nm),Oa20 (" \
-               "940 nm),Oa21 (1020 nm)"
+    bandlist = (
+        "Oa01 (400 nm),Oa02 (412.5 nm),Oa03 (442.5 nm),Oa04 (490 nm),"
+        "Oa05 (510 nm),Oa06 (560 nm),Oa07 (620 nm),Oa08 (665 nm),Oa09"
+        " (673.75 nm),Oa10 (681.25 nm),Oa11 (708.75 nm),Oa12 (753.75 "
+        "nm),Oa13 (761.25 nm),Oa14 (764.375 nm),Oa15 (767.5 nm),Oa16 "
+        "(778.75 nm),Oa17 (865 nm),Oa18 (885 nm),Oa19 (900 nm),Oa20 ("
+        "940 nm),Oa21 (1020 nm)"
+    )
 
     parameters.put("spectralAlbedoTargetBands", bandlist)
 
     # Run the Albedo computation
-    albedo = GPF.createProduct("OLCI.SnowProperties", parameters, prod)
+    albedo = GPF.createProduct("OLCI.SnowProperties", parameters, inprod)
 
     return albedo
 
@@ -229,87 +306,122 @@ def getTiePointGrid_value(inprod, tpg_name, xx, yy):
     return tpg.getPixelFloat(yy, xx)
 
 
-def getS3values(in_file, lat, lon, snow_pollution,
-                pollution_delta, gains=True):
-    """ Read the input S3 file and run the S3 OLCI SNOW processor for a given
-        location.
-        INPUTS:
-        - in_file: Posix path to the xfdumanifest file (in the unzipped
-        downloaded product)
-        - lat: latitude of point to query
-        - lon: longitude of point to query
+def getS3values(in_file, coords, snow_pollution, pollution_delta, gains):
+    """Extract data from S3 SNOW.
+
+    Read the input S3 file and run the S3 OLCI SNOW processor for the
+    coordinates located within the scene.
+
+    Args:
+        in_file (str): Path to a S3 OLCI image xfdumanisfest.xml file
+        coords (list): List of coordinates to extract the data from
+        pollution (bool): S3 SNOW dirty snow flag
+        delta_pol (int): Delta value to consider dirty snow in S3 SNOW
+        gains (bool): Consider vicarious calibration gains
         """
+    # Make a dictionnary to store results
+    stored_vals = {}
+
     # Open SNAP product
     prod = open_prod(in_file)
 
-    # Save resources by working on a small subset around the product.
-    prod_subset, pix_coords = subset(prod, lat, lon)
+    # Loop over coordinates to extract value. Need to think if there is a
+    # quicker way of processing the images...
+    for coord in coords:
 
-    if not prod_subset:
-        out_values = None  # None if location not in product
-    else:
-        # Run the S3 OLCI SNOW processor on the subset
-        snap_albedo = snap_snow_albedo(prod_subset, snow_pollution,
-                                       pollution_delta, gains)
+        # Save resources by working on a small subset around each
+        # coordinates pair contained within the S3 scene. Doesn't process if
+        # the coordinates pair is not in the product
+        prod_subset, pix_coords = subset(prod, coord[1], coord[2])
 
-        # Get RBRR band names and spectral planar bands
-        rbrr_bands = [
-            x for x in list(snap_albedo.getBandNames()) if "BRR" in x
-        ]
-        planar_bands = [
-            x
-            for x in list(snap_albedo.getBandNames())
-            if "spectral_planar" in x
-        ]
-        bb_bands = [x for x in list(snap_albedo.getBandNames())
-                    if "albedo_bb" in x]
-
-        alb_bands = rbrr_bands + planar_bands + bb_bands
-
-        # Extract values from albedo product
-        out_values = {
-            "grain_diameter": None,
-            "ice_indicator": None,
-            "snow_specific_area": None,
-        }
-
-        # Read geometry from the tie point grids
-        vza = getTiePointGrid_value(prod_subset, "OZA", pix_coords[0],
-                                    pix_coords[1])
-        vaa = getTiePointGrid_value(prod_subset, "OAA", pix_coords[0],
-                                    pix_coords[1])
-        saa = getTiePointGrid_value(prod_subset, "SAA", pix_coords[0],
-                                    pix_coords[1])
-        sza = getTiePointGrid_value(prod_subset, "SZA", pix_coords[0],
-                                    pix_coords[1])
-
-        for item in alb_bands:
-            out_values.update({item: None})
-
-        for key in out_values:
-            item = next(
-                x for x in list(snap_albedo.getBandNames()) if key in x
-            )
-            currentband = None
-            currentband = snap_albedo.getBand(item)
-            currentband.loadRasterData()
-            out_values[key] = round(
-                currentband.getPixelFloat(pix_coords[0], pix_coords[1]), 4
+        if not prod_subset:
+            out_values = None  # None if location not in product
+        else:
+            # Fetch the TOA reflectance for the image
+            toa_refl = rad2refl(prod_subset)
+            # Run the S3 OLCI SNOW processor on the subset
+            snap_albedo = snap_snow_albedo(
+                prod_subset, snow_pollution, pollution_delta, gains
             )
 
-        # Update geometry
-        out_values.update({"sza": sza, "vza": vza, "vaa": vaa, "saa": saa})
-
-        # Calculate ndsi at pixel of interest
-        out_values.update({"ndsi": ndsi_pixel(prod_subset, pix_coords)})
-
-        # Add experimental cloud over snow result
-        out_values.update(
-            {
-                "auto_cloud": idepix_cloud(
-                    prod_subset, pix_coords[0], pix_coords[1]
-                )
+            # Extract values from albedo product
+            out_values = {
+                "grain_diameter": None,
+                "ndbi": None,
+                "snow_specific_area": None,
             }
-        )
 
-    return (out_values)
+            # Add band names to extract to the dictionnary
+            rbrr_bands = [
+                x for x in list(snap_albedo.getBandNames()) if "BRR" in x
+            ]
+            planar_bands = [
+                x
+                for x in list(snap_albedo.getBandNames())
+                if "spectral_planar" in x
+            ]
+            bb_bands = [
+                x for x in list(snap_albedo.getBandNames()) if "albedo_bb" in x
+            ]
+            alb_bands = rbrr_bands + planar_bands + bb_bands
+            for item in alb_bands:
+                out_values.update({item: None})
+
+            # Update albedo values
+            for key in out_values:
+                item = next(
+                    x for x in list(snap_albedo.getBandNames()) if key in x
+                )
+                currentband = None
+                currentband = snap_albedo.getBand(item)
+                currentband.loadRasterData()
+                out_values[key] = round(
+                    currentband.getPixelFloat(pix_coords[0], pix_coords[1]), 4
+                )
+
+            # Read geometry from the tie point grids
+            vza = getTiePointGrid_value(
+                prod_subset, "OZA", pix_coords[0], pix_coords[1]
+            )
+            vaa = getTiePointGrid_value(
+                prod_subset, "OAA", pix_coords[0], pix_coords[1]
+            )
+            saa = getTiePointGrid_value(
+                prod_subset, "SAA", pix_coords[0], pix_coords[1]
+            )
+            sza = getTiePointGrid_value(
+                prod_subset, "SZA", pix_coords[0], pix_coords[1]
+            )
+
+            # Update geometry
+            out_values.update({"sza": sza, "vza": vza, "vaa": vaa, "saa": saa})
+
+            # Get TOA Reflectance and update dictionnary
+            toa_refl_bands = list(toa_refl.getBandNames())
+            for bnd in toa_refl_bands:
+                currentband = None
+                currentband = toa_refl.getBand(bnd)
+                currentband.loadRasterData()
+                out_values.update(
+                    {
+                        bnd: round(
+                            currentband.getPixelFloat(
+                                pix_coords[0], pix_coords[1]
+                            ),
+                            4,
+                        )
+                    }
+                )
+
+            # Calculate ndsi at pixel of interest
+            out_values.update({"ndsi": ndsi_pixel(prod_subset, pix_coords)})
+
+            # Add experimental cloud over snow result
+            # out_values.update({"auto_cloud": idepix_cloud(prod_subset,
+            #                    pix_coords[0],
+            #                    pix_coords[1])})
+
+            # Update the full dictionnary
+            stored_vals.update({coord[0]: out_values})
+
+    return stored_vals
